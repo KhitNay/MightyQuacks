37c8b061bbd10776c3ddaf2158babb91526c3959
Khit Nay <khitnay.adrian@gmail.com>
2023-02-08 14:58:59 +1100

    Initial Commit

---
+++ neuralNet.py
@@ -0,0 +1,96 @@
+# Creating a custom classifier based on image imports using pyTorch
+import torch
+import torchvision.transforms as transforms
+from PIL import Image
+import os
+import glob
+import numpy as np
+
+import torch.nn as nn
+import torch.nn.functional as F
+
+# Define the class for the custom dataset
+class CustomImageDataset(torch.utils.data.Dataset):
+    def __init__(self, image_folder, labels_file, transform=None):
+        self.image_folder = image_folder
+        self.labels = {}
+        with open(labels_file, 'r') as f:
+            for line in f:
+                parts = line.strip().split(',')
+                self.labels[parts[0]] = int(parts[1])
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.labels)
+
+    def __getitem__(self, index):
+        image_name = str(index) + '.jpg'
+        image_path = os.path.join(self.image_folder, image_name)
+        image = Image.open(image_path)
+        label = self.labels[image_name]
+
+        if self.transform:
+            image = self.transform(image)
+
+        return image, label
+
+transform = transforms.Compose([
+    transforms.Resize((224, 224)),
+    transforms.ToTensor(),
+    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+])
+
+dataset = CustomImageDataset('path/to/images', 'path/to/labels.txt', transform)
+
+classes = ("left", "right", "straight")
+
+# Define the neural network for the classifier
+class Net(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.conv1 = nn.Conv2d(3, 6, 5)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.fc1 = nn.Linear(16 * 5 * 5, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
+
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = torch.flatten(x, 1) # flatten all dimensions except batch
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+    
+net = Net()
+
+import torch.optim as optim
+
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
+
+for epoch in range(2):  # loop over the dataset multiple times
+
+    running_loss = 0.0
+    for i, data in enumerate(dataset, 0):
+        # get the inputs; data is a list of [inputs, labels]
+        inputs, labels = data
+
+        # zero the parameter gradients
+        optimizer.zero_grad()
+
+        # forward + backward + optimize
+        outputs = net(inputs)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+
+        # print statistics
+        running_loss += loss.item()
+        if i % 2000 == 1999:    # print every 2000 mini-batches
+            print(f'[{epoch + 1}, {i + 1:5d}] loss: {running_loss / 2000:.3f}')
+            running_loss = 0.0
+
+print('Finished Training')
