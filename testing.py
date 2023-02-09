64f45062ba7f92d39b1fd3bd394ddd089dd8a5c5
Khit Nay <khitnay.adrian@gmail.com>
2023-02-09 14:27:50 +1100

    got rid of data on git

---
+++ testing.py
@@ -0,0 +1,13 @@
+# import re
+# s = "l_00454554561134-0.10.jpg"
+# x = "l_00084758755210.00.jpg"
+
+# print(re.findall("-\d.\d\d", s))
+
+# print(re.findall("\d\D\d\d", x))
+
+a = []
+if a == []:
+    print("Hello")
+else:
+    print("world")
