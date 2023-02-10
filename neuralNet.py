93cc87722ca5aee42c627a7f737db80d459d4391
medwards-somhse <medwards@somerville.qld.edu.au>
2023-02-10 14:20:38 +1100

    Let's see what ChatGPT can do

--- neuralNet.py
+++ neuralNet.py
@@ -59,23 +59,48 @@ print("The dataset contains %d images " % datasetSize)
 
 
 # Define the neural network for the classifier
+# class Net(nn.Module):
+#     def __init__(self):
+#         super().__init__()
+#         self.conv1 = nn.Conv2d(3, 6, 5)
+#         self.pool = nn.MaxPool2d(2, 2)
+#         self.conv2 = nn.Conv2d(6, 16, 5)
+#         self.fc1 = nn.Linear(1296, 120)
+#         self.fc2 = nn.Linear(120, 84)
+#         self.fc3 = nn.Linear(84, 3)
+
+#     def forward(self, x):
+#         x = self.pool(F.relu(self.conv1(x)))
+#         x = self.pool(F.relu(self.conv2(x)))
+#         x = torch.flatten(x, 1) # flatten all dimensions except batch
+#         x = F.relu(self.fc1(x))
+#         x = F.relu(self.fc2(x))
+#         x = self.fc3(x)
+#         return x
+
 class Net(nn.Module):
     def __init__(self):
-        super().__init__()
+        super(Net, self).__init__()
         self.conv1 = nn.Conv2d(3, 6, 5)
         self.pool = nn.MaxPool2d(2, 2)
         self.conv2 = nn.Conv2d(6, 16, 5)
-        self.fc1 = nn.Linear(1296, 120)
+        self.fc1 = nn.Linear(16 * 53 * 53, 120)
         self.fc2 = nn.Linear(120, 84)
-        self.fc3 = nn.Linear(84, 3)
+        self.lstm = nn.LSTM(84, 50, 2, batch_first=True)
+        self.fc3 = nn.Linear(50, 3)
+        self.pos_enc = nn.Embedding(10, 50) # position encoding for 10 time steps
 
-    def forward(self, x):
+    def forward(self, x, pos):
         x = self.pool(F.relu(self.conv1(x)))
         x = self.pool(F.relu(self.conv2(x)))
-        x = torch.flatten(x, 1) # flatten all dimensions except batch
+        x = x.view(-1, 16 * 53 * 53)
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
-        x = self.fc3(x)
+        x = x.unsqueeze(1) # add a time step dimension
+        pos = self.pos_enc(pos) # encode the position
+        x = x + pos # add position encoding to feature vector
+        x, (hn, cn) = self.lstm(x)
+        x = self.fc3(x[:, -1, :]) # take the output from the final time step
         return x
     
 net = Net().to(device)
